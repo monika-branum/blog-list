709e450e43e1bac180d0100a8f5fc61a654afe4d
Monika <moncobra@comcast.net>
2023-01-17 14:32:27 -0800

    created useBlogs

--- src/hooks/useBlogs.js
+++ src/hooks/useBlogs.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetchBlogs } from '../services/blogs.js';
 
 export default function useBlogs() {
-  const [blog, setBlog] = useState([]);
-  return <div>useBlogs</div>;
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const resp = await fetchBlogs();
+      setBlogs(resp);
+    };
+    fetchData();
+  }, []);
+  return blogs;
 }
