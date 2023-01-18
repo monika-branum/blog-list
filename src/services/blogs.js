5d5852a1058c06d97784568aa5819e8d940bec90
Monika <moncobra@comcast.net>
2023-01-17 17:17:28 -0800

    deleted console log

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -2,6 +2,5 @@ import { checkError, client } from './client';
 
 export async function fetchBlogs() {
   const resp = await client.from('blogs').select('*');
-  console.log(resp, 'response');
   return checkError(resp);
 }
