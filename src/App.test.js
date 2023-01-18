bdddf8de2e498b9683bd69b87637ba78b015a924
Monika <moncobra@comcast.net>
2023-01-17 17:14:36 -0800

    deleted skip from test

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test.skip('fetches the list of blogs', async () => {
+test('fetches the list of blogs', async () => {
   render(<App />);
   const blog = await screen.findByText(/Maximize Distributed Users/i);
   expect(blog).toBeInTheDocument();
